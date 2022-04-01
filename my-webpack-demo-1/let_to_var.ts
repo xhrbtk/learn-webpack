86f7a41db9f6e10c87087f5a678d9f80895c07a3
xu.hongrui <xu.hongrui@uneed.com>
2022-04-01 15:57:07 +0800

    fix:babel与ast完

--- my-webpack-demo-1/let_to_var.ts
+++ my-webpack-demo-1/let_to_var.ts
@@ -14,6 +14,7 @@
 import { parse } from "@babel/parser"
 import traverse from "@babel/traverse"
 import generate from "@babel/generator"
+import * as babel from '@babel/core'
 // code ->> ast -->> ast2 -->> code2
 const code = `let a = 'let'; let b = 2`
 const ast = parse(code, { sourceType: 'module' })
@@ -21,16 +22,19 @@ const ast = parse(code, { sourceType: 'module' })
 console.log(ast)  // ast 为什么表示源代码 因为是把源代码字符串表示成一个树形结构
 // ast -->> ast2
 // 将ast 里面所有变量声明的let 转换为 var
-traverse(ast, {
-    enter: item => {
-       if( item.node.type  ==  'VariableDeclaration'){
-           if(item.node.kind === 'let'){
-               item.node.kind = 'var'
-           }
-       }
-    }
+// traverse(ast, {
+//     enter: item => {
+//        if( item.node.type  ==  'VariableDeclaration'){
+//            if(item.node.kind === 'let'){
+//                item.node.kind = 'var'
+//            }
+//        }
+//     }
+// })
+// //  将最新的ast 转换为代码
+// // ast2 -->> code2
+// const result = generate(ast, {}, code)
+const result = babel.transformFromAstSync(ast, code, {
+    presets: ['@babel/preset-env']
 })
-//  将最新的ast 转换为代码
-// ast2 -->> code2
-const result = generate(ast, {}, code)
 console.log(result.code)
